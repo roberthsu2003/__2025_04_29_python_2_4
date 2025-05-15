536f08dff2a039e9d4fc10e965e142b70df3f47e
徐國堂 <roberthsu2003@gmail.com>
2025-05-15 13:54:14 +0000

    modify

--- lesson6/lesson6_1.py
+++ lesson6/lesson6_1.py
@@ -1,13 +1,17 @@
 #import tools
 #from tools import calculate_bmi, get_state
-import edu
+#import edu
+#from edu.tools import calculate_bmi, get_state
+#from edu import tools
+from edu.tools import calculate_bmi as a
+from edu.tools import get_state as b
 
 def main():
     # BMI計算
     try:
         height:int = int(input('請輸入你的身高(公分 cm):'))        
         weight = eval(input('請輸入你的體重(公斤 kg):'))        
-        bmi = edu.tools.calculate_bmi(height,weight)
+        bmi = a(height,weight)
     except ValueError:
         print('輸入發生錯誤')
     except Exception as e :
@@ -16,7 +20,7 @@ def main():
         print(f'你的身高:{height} 公分')
         print(f'你的體重:{weight:.2f} 公斤')
         print(f'你的 BMI值為:{bmi:.2f}')
-        print(edu.tools.get_state(bmi))
+        print(b(bmi))
 
 if __name__ == "__main__":
     main()
