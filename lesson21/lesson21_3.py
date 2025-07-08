504250c5dc7ee8bf02bae0b6e5416c8c1e300d00
徐國堂 <roberthsu2003@gmail.com>
2025-07-08 20:20:58 +0800

    modify

--- lesson21/lesson21_3.py
+++ lesson21/lesson21_3.py
@@ -3,5 +3,9 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def index():
     return "<h1>您好, 全世界!</h1>"
+
+@app.route("/name")
+def name():
+    return "<h1>您好, 這是name的頁面!</h1>"
