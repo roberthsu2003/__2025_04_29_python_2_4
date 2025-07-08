fb729938b2b98241d805c824a7faf09486981112
徐國堂 <roberthsu2003@gmail.com>
2025-07-08 19:47:12 +0800

    modify

--- lesson21/lesson21_2.py
+++ lesson21/lesson21_2.py
@@ -6,11 +6,11 @@ def on_button_click():
 def create_main_window():
     window = tk.Tk()
     window.title("簡單Tkinter範例")
-    window.geometry("300x200")
+    window.geometry("500x500")
 
     # 建立按鈕並綁定事件處理函式
-    btn = tk.Button(window, text="點我", command=on_button_click)
-    btn.pack(pady=50)
+    btn = tk.Button(window, text="點我", command=on_button_click, padx=20, pady=20, font=("Arial", 16))
+    btn.pack(pady=100)
 
     return window
 
